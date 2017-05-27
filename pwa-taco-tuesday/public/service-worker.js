83e8b0372eb72f72af2e72cf272e1ae3d3ca7802
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-05-27 14:33:10 -0600

    service worker done

--- pwa-taco-tuesday/public/service-worker.js
+++ pwa-taco-tuesday/public/service-worker.js
@@ -5,3 +5,57 @@ let doCache = false;
 //Name Cache
 let CACHE_NAME = 'taco-tuesday-pwa-cache-v1';
 
+// DELETE OLD CACHE NOT THIS ONE
+self.addEventListener('activate', event => {
+	const cacheWhiteList = [CACHE_NAME];
+	event.waitUnitl(
+		caches.keys()
+		.then(keyList => 
+			Promise.all(keyList.map(key => {
+				if (!cacheWhiteList.includes(key)){
+					console.log('Deleting Cache: ', key);
+					return caches.delete(key)l;
+				}
+			}))
+		)
+	);
+});
+
+// FIRST TIME USER STARTS PWA, 'INSTALL' IS TRIGGERED
+self.addEventListener('install', (event)=> {
+	if (doCache) {
+		event.waitUnitl(
+			caches.open(CACHE_NAME)
+			.then((cache)=> {
+				// GET assets manifest, WebPack Hashes It
+				fetch('asset-manifest.json')
+				.then(res => {
+					res.json();
+				})
+				.then(assets => {
+					// Open Cache and Cache Files
+					// Cache Main page and main js file
+					// Cache static files and/or images
+					const urlsToCache =[
+						'/',
+						assets['main.js']
+					];
+					cache.addAll(urlsToCache);
+					console.log('Cached Files');
+				});
+			})
+		);
+	}
+});
+
+// INTERCEPT REQ AND SERVE FILES
+self.addEventListener('fetch', (e)=>{
+	if (doCache) {
+		event.respondWith(
+			caches.match(e.request)
+			.then((res)=>{
+				return res || fetch(e.request);
+			})
+		);
+	}
+});
