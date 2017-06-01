a00a6e24a43ca1640e01463df91798d1e1f2471d
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-05-31 19:43:04 -0600

    4th heroku bug

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ app.use(forceSSL());
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
-
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
