483ef290e2cec04f950521081aab92264786c20c
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-05-31 19:32:20 -0600

    set up for heroku

--- server.js
+++ server.js
@@ -1,70 +1,40 @@
-// 'use strict'
-// // require express and body parser
-// const express = require('express');
-// const	app = express();
-// const	router = express.Router();
-// const	bodyParser = require('body-parser');
-// const path = require('path');
-// const morgan = require('morgan');
-// const apiRouter = require('./routes/api');
-// const DB = 'mongodb://localhost/tacos';
-// const mongoose = require('mongoose');
+'use strict'
+// for heroku deployment
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// // CONNECT TO DB
-// mongoose.connect(DB, (err) =>{
-// 	if(err) {
-// 		console.log('DB Connection Error');
-// 	}
-// 	console.log('Connected to: ', DB);
-// });
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+//
 
-// // body parser for JSON data middle ware
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(morgan('dev'));
-
-// // API ROUTES
-// app.get('/api', function api_index(req, res){
-// 	res.json({
-// 		message: "Welcome to Taco Tuesday!",
-// 		endpoints: [
-// 		{
-// 			method: 'GET',
-// 			path: '/api/random',
-// 			description: 'Get a random taco'
-// 		},
-// 		{
-// 			method: 'GET', 
-// 			path: '/api/list', 
-// 			description: "show list of tacos and homepage"
-// 		},
-// 		{
-// 			method: 'POST',
-// 			path: '/api/list',
-// 			description: "Add a new taco"
-// 		},
-// 		{
-// 			method: 'SHOW',
-// 			path: '/api/list/:id',
-// 			description: "Show individual taco"
-// 		},
-// 		{
-// 			method: 'PUT',
-// 			path: '/api/list/:id',
-// 			description: "Update your taco"
-// 		},
-// 		{
-// 			method: 'DELETE',
-// 			path: '/api/list/:id',
-// 			description: 'Delete your taco'
-// 		}
-// 		]
-// 	});
-// });
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
 
-// //DB REQUIRE
-// const db = require('./models');
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
 
-// // Route for API
-// app.use('/api', apiRouter);
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
