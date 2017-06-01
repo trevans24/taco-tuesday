947a958d0dfb39aa1d9d6d54af8714d86424dd63
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-05-31 22:26:53 -0600

    removing force ssl for secure server get req

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ const path = require('path');
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+// app.use(forceSSL());
 //
 
 // Run the app by serving the static files
